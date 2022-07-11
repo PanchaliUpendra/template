ff329f9409278c166353eef1841a09c7c2bd71ab
PanchaliUpendra <upendrapanchali83@gmail.com>
2022-07-11 15:03:11 +0530

    created react app modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import Skills from './components/Skills/Skills';
 
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
       <Navbar/>
       <Home />
       <About/>
+      <Skills/>
     </div>
   );
 }
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Skills.css';
+function Skills(){
+    return(
+        <div className='skills'>
+            <div className='skill-head-main'>
+            <div className='skill-head'><h2>Skills</h2></div>
+            </div>
+            <div>
+            <ul className='skill-ul'>
+                <li>HTML</li>
+                <li>CSS3</li>
+                <li>SASS</li>
+                <li>JAVASCRIPT</li>
+                <li>BOOTSTRAP</li>
+                <li>REACT JS</li>
+                <li>NODE JS</li>
+                <li>EXPRESS JS</li>
+                <li>C++</li>
+                <li>JAVA</li>
+                <li>PYTHON</li>
+                <li>DSA</li>
+                <li>MTSQL</li>
+            </ul>
+            </div>
+        </div>
+
+    );
+}
+export default Skills;
