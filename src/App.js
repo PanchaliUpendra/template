a07c3a8bf14d4f78259910fb22b18288a4998c6d
PanchaliUpendra <upendrapanchali83@gmail.com>
2022-07-12 08:18:35 +0530

    modified the education

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
+import Education from './components/Education/Education';
 
 import './App.css';
 
@@ -14,6 +15,7 @@ function App() {
       <Home />
       <About/>
       <Skills/>
+      <Education/>
     </div>
   );
 }
---
+++ src/components/Education/Education.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Education.css';
+function Education(){
+    return(
+        <div className='education'>
+            <div className='edu-head'>
+                <h2 className='edu-head-part'>Education</h2>
+            </div>
+
+        </div>
+
+    );
+}
+export default Education;
